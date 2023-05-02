71186313bcded9d825fb92fb9493ec117e284dce
Karan Chaudhary <tharukaran930@gmail.com>
2023-05-02 16:15:01 +0545

    update

--- sendmail.php
+++ sendmail.php
@@ -6,7 +6,7 @@
     $headers = 'From: cleckfreshmart@gmail.com' . "\r\n" .'Reply-To: cleckfreshmart@gmail.com';
     
     $mail_sent = mail($to, $subject, $message, $headers);
-
+ 
     if(!$mail_sent){
         echo "mail send failed";
     }
