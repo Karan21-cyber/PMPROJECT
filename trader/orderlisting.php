2e2b909d30c715e8b473360025b11288790ce2ae
Karan Chaudhary <tharukaran930@gmail.com>
2023-05-21 23:00:08 +0545

    update

--- trader/orderlisting.php
+++ trader/orderlisting.php
@@ -1,8 +1,9 @@
 <?php
-  include("../db/connection.php");
+include("../db/connection.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +11,14 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="css/orderslis.css">
+    <style>
+        .success {
+            color: green;
+            font-weight: 600;
+        }
+    </style>
 </head>
+
 <body>
 
     <div class="order">
@@ -35,50 +43,75 @@
     </div>
     <div class="user-container">
 
-    <table>
-    <!-- table heading -->
-  
-      <tr>
-        <th>ORDER ID</th>
-        <th>CUSTOMER</th>
-        <th>PRODUCT</th>
-        <th>QTY</th>
-        <th>PRICE(&#163;)</th>
-        <th>DATE</th>
-        <th>STATUS</th>
-        <th>ACTION</th>
-      </tr>
+        <table>
+            <!-- table heading -->
 
-    <tr >
-      <td>3333</td>
-      <td>kARAN CHADUAHRY</td>
-      <td><img id='image' src="../logo/apple2.webp" alt="" /></td>
-      <td>200g</td>
-      <td>200.00</td>
-      <td>3/16/2023</td>
-      <td id="status">Pending...</td>
-      <td> <div class='action'>
-                <a id='approve' href=updatetrader.php?id=$id&action=verified>Delivered</a>
-                
-                <a id='decline' href=deletetrader.php?id=$id&action=decline>Cancelled</a>
-                
-                </div>    </tr>
+            <tr>
+                <th>ORDER ID</th>
+                <th>CUSTOMER</th>
+                <th>PRODUCT</th>
+                <th>QTY</th>
+                <th>PRICE(&#163;)</th>
+                <th>DATE</th>
+                <th>STATUS</th>
+                <th>ACTION</th>
+            </tr>
 
-    <tr>
-        <td>3333</td>
-        <td>kARAN CHADUAHRY</td>
-        <td><img id='image' src="../logo/apple2.webp" alt="" /></td>
-        <td>200g</td>
-        <td>200.00</td>
-        <td>3/16/2023</td>
-        <td id="status">Pending...</td>
-        <td> <div class='action'>
-            <a id='approve' href=updatetrader.php?id=$id&action=verified>Delivered</a>   
-            <a id='decline' href=deletetrader.php?id=$id&action=decline>Cancelled</a>        
-        </div></tr>
+            <?php
 
-  </table>
-</div>
-    
+            $sql = "SELECT o.*,op.*,p.*
+                FROM ORDER_I o
+                JOIN ORDER_PRODUCT op ON o.ORDER_ID = op.ORDER_ID
+                JOIN PRODUCT p ON op.PRODUCT_ID = p.PRODUCT_ID
+                JOIN SHOP s ON p.SHOP_ID = s.SHOP_ID
+                JOIN USER_I u ON s.USER_ID = u.USER_ID
+                WHERE u.USER_ID = :user_id";
+
+            $stid = oci_parse($connection, $sql);
+            oci_bind_by_name($stid, ":user_id", $_SESSION['traderID']);
+            oci_execute($stid);
+
+            while ($row = oci_fetch_array($stid)) {
+                $product_image = $row['PRODUCT_IMAGE'];
+                $order_id = $row['ORDER_ID'];
+                $order_date = $row['ORDER_DATE'];
+                $order_price = $row['TOTAL_PRICE'];
+                $order_status = $row['STATUS'];
+                $order_item = $row['NO_OF_ITEM'];
+
+                echo "
+                <tr>
+                    <td>" . $order_id . "</td>
+                    <td>kARAN CHADUAHRY</td>
+                    <td><img id='image' src='../db/uploads/products/$product_image' alt='' /></td>
+                    <td>" . $order_item . "</td>
+                    <td><b> &pound; " . $order_price . "</b></td>
+                    <td>" . $order_date . "</td>";
+
+                if ($order_status == 'pending') {
+                    echo "<td id='status'>" . $order_status . "</td>";
+                } else {
+                    echo "<td class='success'>" . $order_status . "</td>";
+                }
+
+                if ($order_status == 'pending') {
+                    echo "<td>
+                        <div class='action'>
+                            <a id='decline' href='deletetrader.php?id=$order_id&action=decline'>Remove</a>
+                        </div>
+                        </td>";
+                }
+
+                echo "</tr>";
+            }
+
+            ?>
+
+
+
+        </table>
+    </div>
+
 </body>
+
 </html>
